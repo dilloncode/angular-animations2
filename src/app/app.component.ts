393f409f564ada941cb762464d64f0cbc3c30874
Michael Dillon <mdillon@Jerue.com>
2018-02-14 16:22:48 -0500

    animating the item submissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -38,13 +38,38 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
           style({ opacity: 0, transform: 'translateY(100%)', offset: 1 }),
         ]))
       ])
+    ]),
+
+    trigger('itemEnter', [
+      state('in', style({ transform: 'translateY(0)' })),
+
+      transition('void => *', [
+        style({ transform: 'translateY(-100%', opacity: 0 }),
+        animate('300ms ease-out')
+      ]),
+      transition('* => void', [
+        style({ transform: 'translateY(-100%)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'scaleY(0) translateY(200px)' }))
+      ])
     ])
+
   ]
 })
 export class AppComponent {
   state: string = 'inactive';
 
+  ourItems = [
+    'Start the new app project',
+    'Work on blog',
+    'Lunch at 1'
+  ];
+
   toggleFocus() {
     this.state = this.state === 'inactive' ? 'active' : 'inactive';
+  }
+
+  submitItem(event) {
+    this.ourItems.push(event.target.value);
+    event.target.value = '';
   }
 }
